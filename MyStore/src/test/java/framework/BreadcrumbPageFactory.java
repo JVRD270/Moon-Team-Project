2785aa68d5e17fc30d7adb9ff27727ea118b61c7
Joshua Almonte <jaa75@njit.edu>
2020-12-17 21:11:42 -0500

    Created breadcrumb functionality and super classes

---
+++ MyStore/src/test/java/framework/BreadcrumbPageFactory.java
@@ -0,0 +1,14 @@
+package framework;
+
+public class BreadcrumbPageFactory {
+
+	public static BaseMyStorePage getPageByTitle(String title) {
+		switch(title) {
+			case "Return to Home":
+				return null;
+			default:
+				return null;
+		}	
+	}
+	
+}
